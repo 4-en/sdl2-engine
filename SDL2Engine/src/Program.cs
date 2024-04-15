272d781d818a3f5572116fee1f8b22e9a16ffcba
4-en <ben.schumacher0@gmail.com>
2024-04-15 22:03:21 +0200

    add: init example to Program.cs

--- SDL2Engine/src/Program.cs
+++ SDL2Engine/src/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Starting SDL2 Engine...");
+
+            // create an empty world and add it to the engine
+            var myWorld = new World();
+            var engine = new Engine(myWorld);
+
+
+            // run game loop
+            engine.Run();
+
+            Console.WriteLine("Exiting SDL2 Engine...");
         }
     }
 }

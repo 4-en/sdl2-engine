89c204dcdc7597f1fb0d35b28870894b6e02170f
4-en <ben.schumacher0@gmail.com>
2024-04-16 17:28:50 +0200

    move: Scene class to seperate file

--- SDL2Engine/src/Engine/Base.cs
+++ SDL2Engine/src/Engine/Base.cs
@@ -287,7 +287,6 @@ namespace SDL2Engine
      * and can be overridden to provide custom
      * functionality to GameObjects.
      */
-
     public class Component
     {
         protected GameObject gameObject;
@@ -564,36 +563,6 @@ namespace SDL2Engine
 
     }
 
-
-    public class Scene : GameObject
-    {
-        private Camera camera;
-        public Scene()
-        {
-            this.Position = new Vec2D();
-            this.ParentPosition = new Vec2D();
-            this.Parent = null;
-            this.scene = this;
-            this.camera = new Camera2D(new Vec2D());
-        }
-
-        public Scene(Camera camera)
-        {
-            this.Position = new Vec2D();
-            this.ParentPosition = new Vec2D();
-            this.Parent = null;
-            this.scene = this;
-            this.camera = camera;
-        }
-
-
-        public Camera GetCamera()
-        {
-            return camera;
-        }
-
-
-    }
 
     public class Time
     {
---
+++ SDL2Engine/src/Engine/Scene.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace SDL2Engine
+{
+    public class Scene : GameObject
+    {
+        private Camera camera;
+        public Scene()
+        {
+            this.Position = new Vec2D();
+            this.ParentPosition = new Vec2D();
+            this.Parent = null;
+            this.scene = this;
+            this.camera = new Camera2D(new Vec2D());
+        }
+
+        public Scene(Camera camera)
+        {
+            this.Position = new Vec2D();
+            this.ParentPosition = new Vec2D();
+            this.Parent = null;
+            this.scene = this;
+            this.camera = camera;
+        }
+
+
+        public Camera GetCamera()
+        {
+            return camera;
+        }
+
+
+    }
+}

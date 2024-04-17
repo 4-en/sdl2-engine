6f22fdf921f9cbbf0edbb82a4409f305893a1148
4-en <ben.schumacher0@gmail.com>
2024-04-17 14:40:37 +0200

    add some physics classes

---
+++ SDL2Engine/src/Engine/Physics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2Engine
+{
+
+    public class Collider : Component
+    {
+        // TODO: Implement Collider class
+        // Base class for all colliders
+    }
+
+    public class RaycastHit
+    {
+        // TODO: Implement RaycastHit class
+        // This class should contain information about a collision between a ray and a collider
+        private Collider? collider;
+    }
+    public class Physics
+    {
+        public static bool Raycast(Vec2D origin, Vec2D direction, double distance, out RaycastHit hit)
+        {
+            // TODO: Implement Raycast method
+
+            // Get active scene
+            // Loop through all colliders in the scene
+            // Check for collision between ray and collider
+            // Return true if collision is found
+            // Return false if no collision is found
+            hit = new RaycastHit();
+            return false;
+        }
+    }
+}

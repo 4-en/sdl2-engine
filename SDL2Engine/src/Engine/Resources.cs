a67f2ba2284def18f00bbe9e885ef9d5cc3c3974
4-en <ben.schumacher0@gmail.com>
2024-04-17 19:45:42 +0200

    add ResourceManager

---
+++ SDL2Engine/src/Engine/Resources.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2Engine
+{
+
+    // Resource manager class
+    // This class should be used to load and manage resources such as textures, sounds, etc.
+    // To use Resources, a function should load the resource with the ResourceManager.LoadResource method
+    // This should help to avoid loading the same resource multiple times
+    // The ResourceManager should also be able to preload resources, for example when loading a scene
+    public static class ResourceManager
+    {
+        
+    }
+}

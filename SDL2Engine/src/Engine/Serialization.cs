43352953639c46680eb2e146058c4399196ed4d7
4-en <ben.schumacher0@gmail.com>
2024-04-17 19:50:50 +0200

    add serialization file

---
+++ SDL2Engine/src/Engine/Serialization.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2Engine
+{
+    // Classes and methods for serialization and deserialization of game objects and scenes
+}

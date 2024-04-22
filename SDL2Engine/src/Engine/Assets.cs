8b3b3afb2cc158c6b873401c8d1420de3438c01b
4-en <ben.schumacher0@gmail.com>
2024-04-22 20:10:17 +0200

    rename ResourceManager to AssetManager

--- SDL2Engine/src/Engine/Assets.cs
+++ SDL2Engine/src/Engine/Assets.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 namespace SDL2Engine
 {
 
-    // Resource manager class
+    // Asset manager class
     // This class should be used to load and manage resources such as textures, sounds, etc.
-    // To use Resources, a function should load the resource with the ResourceManager.LoadResource method
+    // To use an asset, a function should load the resource with the ResourceManager.LoadResource method
     // This should help to avoid loading the same resource multiple times
     // The ResourceManager should also be able to preload resources, for example when loading a scene
-    public static class ResourceManager
+    public static class AssetManager
     {
         
     }

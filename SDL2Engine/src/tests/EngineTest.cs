dd059dd8b675abc0bb2d3250ec987d4df674e1f3
4-en <ben.schumacher0@gmail.com>
2024-04-25 21:52:27 +0200

    add base test class

---
+++ SDL2Engine/src/tests/EngineTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2Engine.Testing
+{
+    public abstract class EngineTest
+    {
+        public abstract void Run();
+    }
+}

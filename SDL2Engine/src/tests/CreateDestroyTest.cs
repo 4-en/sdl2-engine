f3ed22bc365e8e19049e1e08fbac644173e34801
4-en <ben.schumacher0@gmail.com>
2024-04-26 02:46:34 +0200

    add test scenario

---
+++ SDL2Engine/src/tests/CreateDestroyTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2Engine.Testing
+{
+
+    /*
+     * This test is used to test the creation and destruction of GameObjects
+     * Make sure that GameObjects are properly destroyed and that they are not leaked
+     * Also watch out for Component leaks
+     * 
+     * Test Script specific listeners like Start, Update, and Destroy
+     */
+    public class CreateDestroyTest
+    {
+
+        // when stopping, check that the counts have the expected values
+        public static int startCount = 0;
+        public static int updateCount = 0;
+        public static int destroyCount = 0;
+        public static int awakeCount = 0;
+        public static int onEnableCount = 0;
+        public static int onDisableCount = 0;
+
+        public void Run()
+        {
+            Console.WriteLine("CreateDestroyTest Run");
+        }
+    }
+}
--- SDL2Engine/src/tests/EngineTest.cs
+++ SDL2Engine/src/tests/EngineTest.cs
@@ -11,4 +11,10 @@ namespace SDL2Engine.Testing
     {
         public abstract void Run();
     }
+
+    // Basically the game engine without any SDL2 calls and no sleep between updates
+    public class TestEngine
+    {
+       
+    }
 }

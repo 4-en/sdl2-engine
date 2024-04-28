e716fa33f5ea3714cb63fbc5234794310aac2ba0
4-en <ben.schumacher0@gmail.com>
2024-04-28 23:10:15 +0200

    add pong project to solution

---
+++ Pong/Program.cs
@@ -0,0 +1,15 @@
+﻿using SDL2Engine;
+
+namespace Pong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scene = new Scene("Pong");
+            var engine = new Engine(scene);
+
+            engine.Run();
+        }
+    }
+}

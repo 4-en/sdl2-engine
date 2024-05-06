c97b515cb77dbaa65d4bff407802701c7116bb30
4-en <benschumacher0@gmail.com>
2024-05-06 13:37:36 +0200

    add sdl2 mixer to install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -14,6 +14,9 @@ sudo apt install -y libsdl2-image-dev
 # Install SDL2_ttf development library
 sudo apt install -y libsdl2-ttf-dev
 
+# Install SDL2_mixer development library
+sudo apt install -y libsdl2-mixer-dev
+
 echo "SDL2 and its extensions SDL2_image and SDL2_ttf have been installed."
 
 # Install git submodules

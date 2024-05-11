36904b6725478c68f66b8f7db8bb3449117582a5
max061201 <104463069+max061201@users.noreply.github.com>
2024-05-11 22:45:45 +0200

    Home screen hover over text bigger

--- Pong/src/HomeScreen.cs
+++ Pong/src/HomeScreen.cs
@@ -3,23 +3,26 @@ using SDL2Engine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using static SDL2.SDL;
 using static System.Formats.Asn1.AsnWriter;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Pong.src
 {
     internal class HomeScreen
     {
-        
+
 
         public static Scene CreateScene()
         {
             var scene = new Scene("Home Screen");
             Vec2D gameBounds = new Vec2D(1920, 1080);
 
-            using (var activeScene = scene.Activate()) {
+            using (var activeScene = scene.Activate())
+            {
                 GameObject gameTitle = HomeScreenText("Pong", gameBounds.x / 2, 200, 200);
                 GameObject level1 = HomeScreenText("Level 1", gameBounds.x / 2, 500, 100);
                 GameObject level2 = HomeScreenText("Level 2", gameBounds.x / 2, 600, 100);
@@ -40,7 +43,7 @@ namespace Pong.src
 
         private static GameObject HomeScreenText(string text, double x, double y, int fontSize)
         {
-            
+
             var textObject = new GameObject(text);
             textObject.transform.position = new Vec2D(x, y);
             var textComponent = textObject.AddComponent<TextRenderer>();
@@ -60,14 +63,67 @@ namespace Pong.src
     class MenuMouseTracker : Script
     {
         public event Action<string> OnClick;
+        private int originalFontSize = 100;
+        private bool isMouseOver;
+        private TextRenderer textRenderer;
 
+        public override void Start()
+        {
+            textRenderer = gameObject.GetComponent<TextRenderer>();
+            // originalFontSize = textRenderer.GetFontSize();
+        }
         public override void Update()
         {
+            Vec2D mousePosition = Input.GetMousePosition();
+            GameObject gameObject = this.gameObject;
+            Camera camera = Camera.GetCamera(gameObject);
+
+            if (camera != null)
+            {
+                mousePosition = camera.ScreenToWorld(mousePosition);
+            }
+
+            Vec2D position = gameObject.transform.position;
+            Rect rect = new Rect(position.x - 200, position.y - 50, 400, 100);
+
+            if (rect.Contains(mousePosition))
+            {
+
+                // Mouse is over the text
+                if (!isMouseOver)
+                {
+                    // Change text size when mouse enters
+                    isMouseOver = true;
+
+                    string text = textRenderer.GetText();
+                    if (text != "Pong")
+                    {
+                        textRenderer.SetFontSize(originalFontSize + 10); // Adjust the size increase as needed
+                    }
+                }
+            }
+            else
+            {
+                // Mouse is not over the text
+                if (isMouseOver)
+                {
+                    string text = textRenderer.GetText();
+
+                    // Restore original size when mouse leaves
+                    isMouseOver = false;
+                    if (text != "Pong")
+                    {
+                        textRenderer.SetFontSize(originalFontSize); // Adjust the size increase as needed
+
+                    }
+                }
+            }
+
+
+
             if (Input.GetMouseButtonDown(0))
             {
-                Vec2D mousePosition = Input.GetMousePosition();
-                GameObject gameObject = this.gameObject;
-                Camera camera = Camera.GetCamera(gameObject);
+
 
                 if (camera != null)
                 {
@@ -76,13 +132,11 @@ namespace Pong.src
 
                 TextRenderer textRenderer = gameObject.GetComponent<TextRenderer>();
 
-                // Get the position and dimensions of the text object
-                Vec2D position = gameObject.transform.position;
-                Rect rect = new Rect(position.x-200, position.y-50, 400, 100);
-                
-                 if (rect.Contains(mousePosition))
+
+
+                if (rect.Contains(mousePosition))
                 {
-                    if(gameObject.GetName().Equals("Level 1"))
+                    if (gameObject.GetName().Equals("Level 1"))
                     {
                         SceneManager.SetScene(LevelManager.CreateLevel1());
                     }
@@ -98,6 +152,7 @@ namespace Pong.src
                     {
                         SceneManager.SetScene(LevelManager.CreateLevel4());
                     }
+
                 }
 
             }

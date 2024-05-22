fb6ec130b2295cf7ab939f2acf20f821db84e180
4-en <ben.schumacher0@gmail.com>
2024-05-22 20:15:30 +0200

    remove old test

---
+++ SDL2Engine/src/tests/SerializationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2Engine.tests
+{
+    internal class SerializationTest
+    {
+
+        public static Scene CreateScene()
+        {
+            Scene scene = new Scene();
+            GameObject gameObject = new GameObject();
+            scene.AddGameObject(gameObject);
+            return scene;
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine("SerializationTest Run");
+        }
+    }
+}

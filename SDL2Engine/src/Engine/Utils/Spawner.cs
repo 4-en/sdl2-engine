440244cf998db0e816711cbe6cef8a12fa5a28fa
4-en <ben.schumacher0@gmail.com>
2024-05-23 19:03:23 +0200

    add spawner for prototypes

---
+++ SDL2Engine/src/Engine/Utils/Spawner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2Engine.Utils
+{
+    public class Spawner : Script
+    {
+        public Prototype? spawnPrototype;
+        public int minSpawnAmount = 1;
+        public int maxSpawnAmount = 1;
+        public virtual int GetSpawnAmount()
+        {
+            return random.Next(minSpawnAmount, maxSpawnAmount + 1);
+        }
+
+        public double spawnRadius = 0;
+        public virtual Vec2D GetSpawnPosition()
+        {
+            var angle = random.NextDouble() * Math.PI * 2;
+            var offset = new Vec2D(Math.Cos(angle), Math.Sin(angle)) * spawnRadius;
+
+            return this.gameObject.GetPosition() + offset;
+        }
+
+        public double spawnInterval = 10;
+        public double lastSpawnTime = 0;
+        public virtual bool CanSpawn()
+        {
+            if (lastSpawnTime + spawnInterval < Time.time)
+            {
+                lastSpawnTime = Time.time;
+                return true;
+            }
+            return false;
+        }
+
+        public override void Update()
+        {
+            if (spawnPrototype == null)
+            {
+                return;
+            }
+
+            if (CanSpawn())
+            {
+                for (int i = 0; i < GetSpawnAmount(); i++)
+                {
+                    var spawn = spawnPrototype.Instantiate();
+                    spawn.SetPosition(GetSpawnPosition());
+                }
+            }
+        }
+
+    }
+}

7472da3d4825b8be43e4bb910ac1d62986b23d9c
4-en <ben.schumacher0@gmail.com>
2024-05-24 12:52:39 +0200

    change damage from double to Damage struct

--- ShootEmUp/src/Entities/General.cs
+++ ShootEmUp/src/Entities/General.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SDL2Engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,41 @@ using System.Threading.Tasks;
 
 namespace ShootEmUp.Entities
 {
+    public enum Team
+    {
+        Player,
+        Enemy,
+        Neutral
+    }
+    public struct Damage
+    {
+        public double Value;
+        public GameObject? Source;
+        public Team Team = Team.Enemy;
+
+        public Damage(double value)
+        {
+            Value = value;
+        }
+
+        public Damage(double value, GameObject source)
+        {
+            Value = value;
+            Source = source;
+        }
+
+        public Damage(double value, GameObject source, Team team)
+        {
+            Value = value;
+            Source = source;
+            Team = team;
+        }
+    }
     public interface IDamageable
     {
-        void Damage(double damage);
-        void Heal(double heal);
+        void Damage(Damage damage);
+
+        void Heal(double value);
     }
 
 

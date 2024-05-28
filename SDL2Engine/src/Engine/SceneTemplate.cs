c0080803f97f0d97626cd8575317b84bf253de95
4-en <ben.schumacher0@gmail.com>
2024-05-28 17:37:19 +0200

    add SceneTemplate class

---
+++ SDL2Engine/src/Engine/SceneTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml;
+
+namespace SDL2Engine
+{
+    /*
+     * SceneTemplates are used to load GameObjects from a file.
+     * The GameObjects are created by using named Prototype objects.
+     * Additionally, the GameObjects can have custom attributes set,
+     * for example, the position of the GameObject.
+     */
+    public class SceneTemplate
+    {
+
+    }
+}

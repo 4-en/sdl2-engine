c9edee06872e4926d64154be90677e5a8a51b25e
max061201 <104463069+max061201@users.noreply.github.com>
2024-06-02 22:01:11 +0200

    Background Parallax effect

--- ShootEmUp/src/Background.cs
+++ ShootEmUp/src/Background.cs
@@ -7,13 +7,13 @@ namespace ShootEmUp
     {
         protected Vec2D gameBounds = new Vec2D(1920, 1080);
         public int BgTileSize = 1024;
+        private List<(GameObject background, Vec2D originalPosition)>? prototype = new List<(GameObject, Vec2D)>();
 
         public override void Start()
         {
             int rows = 3; // Anzahl der Reihen
             int columns = 4; // Anzahl der Spalten
-            float spacing = BgTileSize - 1; // Abstand zwischen den Objekten bzw png size
-
+            float spacing = BgTileSize - 1; // Abstand zwischen den Objekten bzw. PNG-Größe
 
             for (int row = 0; row < rows; row++)
             {
@@ -34,14 +34,35 @@ namespace ShootEmUp
                     if (rows % 2 == 0) yOffset += spacing / 2;
 
                     // Setze die Position relativ zur Mitte des Spielfelds
-                    background.transform.position = new Vec2D((gameBounds.x / 2) + xOffset, (gameBounds.y / 2) + yOffset);
+                    Vec2D position = new Vec2D((gameBounds.x / 2) + xOffset, (gameBounds.y / 2) + yOffset);
+                    background.transform.position = position;
+                    prototype?.Add((background, position));
                 }
             }
         }
+
         public override void Update()
         {
+            var camera = GetCamera() as Camera;
 
+            if (camera == null || prototype == null) return;
+
+            Vec2D cameraPosition = camera.GetPosition();
+
+            foreach (var (background, originalPosition) in prototype)
+            {
+                if (background != null)
+                {
+                    // Berechne die neue Position des Hintergrundobjekts relativ zur Kameraposition
+                    float xOffset = (float)(originalPosition.x - (gameBounds.x / 2));
+                    float yOffset = (float)(originalPosition.y - (gameBounds.y / 2));
+
+                    // Setze die Position relativ zur Kameraposition
+                    background.transform.position = new Vec2D((cameraPosition.x / 1.5) + xOffset, (cameraPosition.y / 1.5) + yOffset);
+                }
+            }
         }
+
 
     }
 }

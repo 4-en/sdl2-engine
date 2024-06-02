d83a7111f223dab98c175d70c1b5d5cfc8457182
4-en <ben.schumacher0@gmail.com>
2024-06-02 20:47:33 +0200

    remove unused imports

--- ShootEmUp/src/LevelManager.cs
+++ ShootEmUp/src/LevelManager.cs
@@ -1,10 +1,5 @@
 ﻿using SDL2Engine;
 using ShootEmUp.Level;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ShootEmUp
 {

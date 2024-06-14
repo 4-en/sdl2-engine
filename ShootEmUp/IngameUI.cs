708093983db6c4ef4e33002370f94c737b843330
LunesH <105421376+LunesH@users.noreply.github.com>
2024-06-14 12:40:36 +0200

    IngameUI class

---
+++ ShootEmUp/IngameUI.cs
@@ -0,0 +1,243 @@
+﻿using SDL2Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SDL2.SDL;
+
+namespace ShootEmUp
+{
+    internal class IngameUI
+    {
+        internal class UserInterface : Script
+        {
+
+            public override void Start()
+            {
+                AddComponent<HealthBar>();
+                AddComponent<HighScore>();
+                AddComponent<MoneyIndicator>();
+                AddComponent<HealthReducer>();
+                AddComponent<HighscoreUpdater>();
+                AddComponent<MoneyUpdater>();
+
+
+            }
+        }
+
+        internal class HealthReducer : Script
+        {
+            public override void Update()
+            {
+                //key inputs
+                if (Input.GetKeyPressed((int)SDL_Keycode.SDLK_1))
+                {
+                    if (Player.currentHealth > 0)
+                    {
+
+                        Player.currentHealth -= 1;
+                    }
+                }
+                if (Input.GetKeyPressed((int)SDL_Keycode.SDLK_2))
+                {
+                    if (Player.currentHealth < Player.maxHealth)
+                    {
+                        Player.currentHealth += 1;
+                    }
+                }
+
+            }
+        }
+
+        internal class HighscoreUpdater : Script
+        {
+            public override void Update()
+            {
+                //key inputs
+                if (Input.GetKeyDown((int)SDL_Keycode.SDLK_3))
+                {
+                    PlayerData.Instance.TotalScore += 100;
+                }
+                if (Input.GetKeyDown((int)SDL_Keycode.SDLK_4))
+                {
+                    PlayerData.Instance.TotalScore -= 100;
+                }
+            }
+        }
+
+        internal class MoneyUpdater : Script
+        {
+            public override void Update()
+            {
+                //key inputs
+                if (Input.GetKeyDown((int)SDL_Keycode.SDLK_5))
+                {
+                    PlayerData.Instance.Money += 100;
+                }
+                if (Input.GetKeyDown((int)SDL_Keycode.SDLK_6))
+                {
+                    PlayerData.Instance.Money -= 100;
+                }
+            }
+        }
+
+        internal class MoneyIndicator : Script
+        {
+
+            GameObject moneyIndicator = new GameObject("HighscoreText");
+
+
+            public override void Start()
+            {
+
+                //set player highscore
+                Player.displayedHighscore = PlayerData.Instance.TotalScore;
+
+
+                var text = moneyIndicator.AddComponent<TextRenderer>();
+                moneyIndicator.transform.position = new Vec2D(1920 - 100, 100);
+                text.anchorPoint = AnchorPoint.CenterRight;
+                text.SetText(PlayerData.Instance.Money.ToString());
+                text.SetColor(SDL2Engine.Color.White);
+                text.SetFontSize(48);
+                text.SetFontPath("Assets/Fonts/PressStartRegular.ttf");
+
+            }
+
+            public override void Update()
+            {
+
+                if (Player.displayedMoney < PlayerData.Instance.Money)
+                {
+                    Player.displayedMoney += 1;
+                }
+                else if (Player.displayedMoney > PlayerData.Instance.Money)
+                {
+                    Player.displayedMoney -= 1;
+                }
+
+                //update moneyIndicator
+                var text = moneyIndicator.GetComponent<TextRenderer>();
+                text?.SetText(Player.displayedMoney.ToString() + "$");
+
+            }
+        }
+
+        internal class HighScore : Script
+        {
+
+            GameObject highscoreText = new GameObject("HighscoreText");
+
+
+            public override void Start()
+            {
+
+                //set player highscore
+                Player.displayedHighscore = PlayerData.Instance.TotalScore;
+
+
+                var text = highscoreText.AddComponent<TextRenderer>();
+                highscoreText.transform.position = new Vec2D(1920 / 2, 100);
+                text.anchorPoint = AnchorPoint.Center;
+                text.SetText(PlayerData.Instance.TotalScore.ToString());
+                text.SetColor(SDL2Engine.Color.White);
+                text.SetFontSize(48);
+                text.SetFontPath("Assets/Fonts/PressStartRegular.ttf");
+
+            }
+
+            public override void Update()
+            {
+
+                if (Player.displayedHighscore < PlayerData.Instance.TotalScore)
+                {
+                    Player.displayedHighscore += 1;
+                }
+                else if (Player.displayedHighscore > PlayerData.Instance.TotalScore)
+                {
+                    Player.displayedHighscore -= 1;
+                }
+
+                //update highscoreText
+                var text = highscoreText.GetComponent<TextRenderer>();
+                text?.SetText(Player.displayedHighscore.ToString());
+
+            }
+        }
+
+
+        internal class HealthBar : Script
+        {
+            GameObject healthBarBackground = new GameObject("HealthBarBackground");
+            GameObject healthBarBorder = new GameObject("HealthBarBorder");
+            GameObject healthBarText = new GameObject("HealthBarText");
+            int width = 300;
+            int height = 60;
+
+
+            public override void Start()
+            {
+
+
+                healthBarBackground.transform.position = new Vec2D(100, 1080 - 100);
+                var healthIndicator = healthBarBackground.AddComponent<TextRenderer>();
+                healthIndicator.anchorPoint = AnchorPoint.CenterLeft;
+                healthIndicator.SetPreferredSize(new Rect(0, 0, width, height));
+                healthIndicator.SetColor(SDL2Engine.Color.Green);
+                healthIndicator.SetBackgroundColor(new SDL2Engine.Color(255, 0, 0, 255));
+
+
+
+                var border = healthBarBorder.AddComponent<TextRenderer>();
+                var textRenderHelper = healthBarBorder.AddComponent<TextRenderHelper>();
+                healthBarBorder.transform.position = new Vec2D(100, 1080 - 100);
+                border.anchorPoint = AnchorPoint.CenterLeft;
+                border.SetPreferredSize(new Rect(0, 0, width, height));
+                border.SetBorderSize(2);
+                border.SetBorderColor(SDL2Engine.Color.Black);
+
+                var text = healthBarText.AddComponent<TextRenderer>();
+                healthBarText.transform.position = new Vec2D(100 + width / 2, 1080 - 100);
+                text.anchorPoint = AnchorPoint.Center;
+                text.SetText(Player.currentHealth.ToString());
+                text.SetColor(SDL2Engine.Color.White);
+                text.SetFontSize(38);
+                text.SetFontPath("Assets/Fonts/Arcadeclassic.ttf");
+
+                var titleText = new GameObject("TitleText");
+                var text2 = titleText.AddComponent<TextRenderer>();
+                titleText.transform.position = new Vec2D(100 + width / 2, 1080 - 150);
+                text2.anchorPoint = AnchorPoint.Center;
+                text2.SetText("Health");
+                text2.SetColor(SDL2Engine.Color.Red);
+                text2.SetFontSize(50);
+                text2.SetFontPath("Assets/Fonts/Arcadeclassic.ttf");
+
+
+                var heart = new GameObject("Heart");
+                var heartTexture = heart.AddComponent<TextureRenderer>();
+                heartTexture.relativeToCamera = false;
+                heartTexture.SetSource("Assets/Textures/health.png");
+                heartTexture.IsVisible(new Rect(4000, 4000));
+                heart.transform.position = new Vec2D(110, 1080 - 100);
+
+            }
+
+            public override void Update()
+            {
+                double currentHealth = Player.currentHealth;
+                double maxHealth = Player.maxHealth;
+                double healthBarWidth = currentHealth / maxHealth * width;
+                var healthIndicator = healthBarBackground.GetComponent<TextRenderer>();
+                healthIndicator?.SetRect(new Rect(0, 0, healthBarWidth, height));
+
+                //update the text
+                var text = healthBarText.GetComponent<TextRenderer>();
+                text?.SetText(Player.currentHealth.ToString());
+
+            }
+        }
+
+    }
+}

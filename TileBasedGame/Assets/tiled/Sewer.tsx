0136933d138d02472d7f64810a92b26755bfc364
4-en <ben.schumacher0@gmail.com>
2024-06-18 15:56:19 +0200

    add some files for testing

---
+++ TileBasedGame/Assets/tiled/Sewer.tsx
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Sewer" tilewidth="16" tileheight="16" tilecount="352" columns="16">
+ <image source="../Textures/tilesets/sewer_1.png" width="256" height="352"/>
+ <tile id="0" type="Obstacle"/>
+ <tile id="1" type="Obstacle"/>
+ <tile id="2" type="Obstacle"/>
+ <tile id="3" type="Obstacle"/>
+ <tile id="4" type="Obstacle"/>
+ <tile id="16" type="Obstacle"/>
+ <tile id="17" type="Obstacle"/>
+ <tile id="18" type="Obstacle"/>
+ <tile id="19" type="Obstacle"/>
+ <tile id="20" type="Obstacle"/>
+ <tile id="32" type="Obstacle"/>
+ <tile id="33" type="Obstacle"/>
+ <tile id="34" type="Obstacle"/>
+ <tile id="35" type="Obstacle"/>
+ <tile id="36" type="Obstacle"/>
+ <tile id="48" type="Obstacle"/>
+ <tile id="49" type="Obstacle"/>
+ <tile id="50" type="Obstacle"/>
+ <tile id="55" type="Obstacle"/>
+ <tile id="56" type="Obstacle"/>
+ <tile id="57" type="Obstacle"/>
+ <tile id="58" type="Obstacle"/>
+ <tile id="64" type="Obstacle"/>
+ <tile id="65" type="Obstacle"/>
+ <tile id="66" type="Obstacle"/>
+ <tile id="71" type="Obstacle"/>
+ <tile id="72" type="Obstacle"/>
+ <tile id="73" type="Obstacle"/>
+ <tile id="74" type="Obstacle"/>
+ <tile id="84" type="Obstacle"/>
+ <tile id="85" type="Obstacle"/>
+ <tile id="86" type="Obstacle"/>
+ <wangsets>
+  <wangset name="Stone1" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="2" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="32" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="34" wangid="0,0,1,1,1,0,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>

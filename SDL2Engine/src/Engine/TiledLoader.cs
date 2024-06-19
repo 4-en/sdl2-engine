a419043fc1bc6a6b004d8bafc91fea1ee83d1e0f
4-en <ben.schumacher0@gmail.com>
2024-06-19 21:14:14 +0200

    add loading from Tiled dir

--- SDL2Engine/src/Engine/TiledLoader.cs
+++ SDL2Engine/src/Engine/TiledLoader.cs
@@ -4,9 +4,20 @@ namespace SDL2Engine
 {
     public static class TiledLoader
     {
+
+        private static string AdjustPath(string path, string default_path)
+        {
+            // check if there is a / or \\ in the path
+            if (path.Contains("/") || path.Contains("\\"))
+            {
+                return path;
+            }
+
+            return default_path + path;
+        }
         public static List<GameObject> LoadTMX(string path)
         {
-
+            path = AdjustPath(path, "Assets/Tiled/");
 
             return new List<GameObject>();
         }
